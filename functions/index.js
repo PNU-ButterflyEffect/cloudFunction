21ca11837fc2dbbd15cc7097f120a4bd8c682557
soicem <knq512412@gmail.com>
2018-08-29 20:01:53 +0900

    push noti base complete with seismic info

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,6 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
-
 admin.initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
@@ -19,6 +18,11 @@ admin.initializeApp();
   });
 });*/
 
+var building_info = null
+var seismicDesign = null // 내진설계여부
+var address = null
+var safetyInfo =null
+var minMeter = 1000000
 const haversine = require('haversine') 
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
@@ -28,43 +32,66 @@ exports.noti = functions.database.ref('/UserLocation/{UID}/{pushId}/')
       // Grab the current value of what was written to the Realtime Database.
       const original = snapshot.val();
       //console.log('Uppercasing', context.params.UID, context.params.pushId, original);
-      
+      //console.log(haversine(start, end, {unit: 'meter'}))
       const start = {
         latitude: original.latitude,
         longitude: original.longitude
       }
       
-      const end = {
-        latitude: 27.950575,
-        longitude: -82.457178
+      var end = {
+        latitude: null,
+        longitude: null
       }
       
-      console.log(haversine(start, end, {unit: 'meter'}))
-      const uppercase = "100";
-  
+      var tmp = {
+        latitude: null,
+        longitude: null
+      }
+      
       var ref = admin.database().ref(`Users/${context.params.UID}/playerId`);
       var ref2 = admin.database().ref('building_info');
       
-      var safetyInfo = "되어있음"
-      var enteredTime = "~"
-      
       ref2.once("value", function(snapshot){
-        console.log(snapshot.val()
+        building_info = snapshot.val()
+        for(x in building_info){    
+          //console.log(x['EPSG_4326_Y'] + " / " + tmp['longitude'])
+          tmp['latitude'] = building_info[x]['EPSG_4326_Y']
+          tmp['longitude'] = building_info[x]['EPSG_4326_X']
+          var result = haversine(start, tmp, {unit: 'meter'})
+          //console.log("내진설계여부 : " + result + "/" + minMeter)
+          //console.log(result < minMeter)
+          if(result < minMeter){
+            minMeter = result
+            end['latitude'] = tmp['latitude']
+            end['longitude'] = tmp['longitude']
+            seismicDesign = building_info[x]['내진설계여부']
+            address = building_info[x]['주소']
+            console.log(minMeter)
+          }
+        }
       })
+
+      if(seismicDesign === 'O'){
+        safetyInfo = "되어있음"
+      } else if(seismicDesign === 'X'){
+        safetyInfo = "되어있지않음"
+      }else{
+        safetyInfo = "정보없음"
+      } 
+      console.log("최소 거리 : " + minMeter)
       return ref.once("value", function(snapshot){
-          const payload = {
-                notification: {
-                  title: '들어온 시간 : ' + original.longitude + '/' + original.latitude,
-                  body: '내진설계 여부 :' +safetyInfo + '\n자세한 건물 정보를 보려면 탭하세요.'
-                }
-              };
-              console.log(snapshot.val())
-              admin.messaging().sendToDevice(snapshot.val(), payload)
-      
-              },
-          function (errorObject) {
-              console.log("The read failed: " + errorObject.code);
+        const payload = {
+              notification: {
+                title: '들어온 시간 : ' + original.createdTime  ,
+                body: '내진설계 여부 :' + safetyInfo + '\n자세한 건물 정보를 보려면 탭하세요.'+ "\n" + address
+              }
+            };
+            //console.log(snapshot.val())
+        admin.messaging().sendToDevice(snapshot.val(), payload)
+    
+            },
+        function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
       });
-      return snapshot.ref.child('date').set(uppercase);
     });
 

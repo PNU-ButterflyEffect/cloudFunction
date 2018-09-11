68985b73d15981e88f3d448cb770240909652696
soicem <knq512412@gmail.com>
2018-09-12 01:48:35 +0900

    add delay for referencing UserLocation

--- functions/index.js
+++ functions/index.js
@@ -22,77 +22,105 @@ var building_info = null
 var seismicDesign = null // 내진설계여부
 var address = null
 var safetyInfo =null
-var minMeter = 100 // 10meter
+var minMeter = 1000 // 10meter
+
 const haversine = require('haversine') 
+var sleep = require('system-sleep');
 var ref2 = admin.database().ref('building_info');
 ref2.once("value", function(snapshot){
   building_info = snapshot.val()
 })
-exports.noti = functions.database.ref('/UserLocation/{UID}/{pushId}/')
+exports.pushNoti = functions.database.ref('/UserLocation/{UID}/{pushId}/')
     .onCreate((snapshot, context) => {
       
       // Grab the current value of what was written to the Realtime Database.
-      const original = snapshot.val();
+      //const original = snapshot.val(); // snapshot is unstable
+      console.log(context.params.UID)
+      console.log(context.params.pushId)
+      console.log("soicem : " + snapshot.val())
+      console.log("sleep now")
+      sleep(500); // 0.5 seconds
+      var newLocationRef1 = admin.database().ref('UserLocation/' + context.params.UID + '/' + context.params.pushId);
+      newLocationRef1.once("value", function(snapshot){console.log(snapshot.val())})
+      var newLocationRef = admin.database().ref('UserLocation/' + context.params.UID + '/' + context.params.pushId);
+      newLocationRef.once("value", function(snapshot){
+        original = snapshot.val()
+        console.log(original)
+        console.log(context)
+      
+      
       //console.log('Uppercasing', context.params.UID, context.params.pushId, original);
       //console.log(haversine(start, end, {unit: 'meter'}))
-      const start = {
-        latitude: original.latitude,
-        longitude: original.longitude
-      }
-      
-      var end = {
-        latitude: null,
-        longitude: null
-      }
-      
-      var tmp = {
-        latitude: null,
-        longitude: null
-      }
-      
-      var ref = admin.database().ref(`Users/${context.params.UID}/playerId`);
-      
-      for(x in building_info){    
-        //console.log(x['EPSG_4326_Y'] + " / " + tmp['longitude'])
-        tmp['latitude'] = building_info[x]['EPSG_4326_Y']
-        tmp['longitude'] = building_info[x]['EPSG_4326_X']
-        var result = haversine(start, tmp, {unit: 'meter'})
-        //console.log("내진설계여부 : " + result + "/" + minMeter)
-        //console.log(result < minMeter)
-        if(result < minMeter){
-          minMeter = result
-          end['latitude'] = tmp['latitude']
-          end['longitude'] = tmp['longitude']
-          seismicDesign = building_info[x]['내진설계여부']
-          address = building_info[x]['주소']
-          //console.log(minMeter)
+        const start = {
+          latitude: original.latitude,
+          longitude: original.logitude
         }
-      }
-      
-      if(seismicDesign === 'O'){
-        safetyInfo = "내진설계가 되어있습니다."
-      } else if(seismicDesign === 'X'){
-        safetyInfo = "내진설계가 되어있지않습니다."
-      } else if(seismicDesign == null){
-        seismicDesign = seismicDesign // 넌너고 난나야
-      }else{
-        safetyInfo = "정보없음"
-      } 
-      console.log("최소 거리 : " + minMeter)
-      return ref.once("value", function(snapshot){
-          if(seismicDesign != null){
-            const payload = {
-              notification: {
-                title: '들어온 시간 : ' + original.createdTime  ,
-                body: safetyInfo + '\n자세한 건물 정보를 보려면 탭하세요.'+ "\n" + address.split('/')[1]
-              }
-            };
-            //console.log(snapshot.val())
-            admin.messaging().sendToDevice(snapshot.val(), payload)
+        //console.log(start)
+        var end = {
+          latitude: null,
+          longitude: null
+        }
+        
+        var tmp = {
+          latitude: null,
+          longitude: null
+        }
+        
+        var ref = admin.database().ref(`Users/${context.params.UID}/playerId`);
+        
+        for(x in building_info){    
+          //console.log(x['EPSG_4326_Y'] + " / " + tmp['longitude'])
+          tmp['latitude'] = building_info[x]['EPSG_4326_Y']
+          tmp['longitude'] = building_info[x]['EPSG_4326_X']
+          //console.log(tmp)
+          //console.log(start)
+          var result = haversine(start, tmp, {unit: 'meter'})
+          //console.log(result)
+          //console.log("내진설계여부 : " + result + "/" + minMeter)
+          //console.log(result < minMeter)
+          //console.log(result)
+          if(result < minMeter){
+            minMeter = result
+            end['latitude'] = tmp['latitude']
+            end['longitude'] = tmp['longitude']
+            seismicDesign = building_info[x]['내진설계여부']
+            address = building_info[x]['주소']
+            //console.log(minMeter)
           }
-        },
-        function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-      });
+        }
+        
+        if(seismicDesign === 'O'){
+          safetyInfo = "내진설계가 되어있습니다."
+        } else if(seismicDesign === 'X'){
+          safetyInfo = "내진설계가 되어있지않습니다."
+        }else{
+          safetyInfo = "정보없음"
+        } 
+
+        console.log("최소 거리 : " + minMeter + safetyInfo)
+        console.log(original.createdTime)
+        console.log(original)
+        console.log(address)
+        return ref.once("value", function(snapshot){
+            if(safetyInfo != null){
+              const payload = {
+                notification: {
+                  title: '들어온 시간 : ' + original.createdTime  ,
+                  body: safetyInfo + '\n자세한 건물 정보를 보려면 탭하세요.'+ "\n" + address.split('/')[1]
+                }
+              };
+              //console.log(snapshot.val())
+              admin.messaging().sendToDevice(snapshot.val(), payload)
+            }
+          },
+          function (errorObject) {
+              console.log("The read failed: " + errorObject.code);
+        });
+      })
+      /*var ref = firebase.database().ref("dinosaurs");
+      ref.orderByKey().on("child_added", function(snapshot) {
+        console.log(snapshot.key);
+      });*/
+      return 0
     });
 
